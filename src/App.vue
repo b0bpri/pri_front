a1d9b0fdca32bf1e4766d39d55a3f9b8b29d9366
s434807 <bwisniewski887@gmail.com>
2025-12-17 20:58:17 +0100

    Added Notivue library for toast notifications. Added NotivueNotification component to with functions pushNotification and pushPromiseNotification to be used for toast-styled notifications in the app.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <NavBar />
     <router-view />
+    <NotivueNotification />
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import NotivueNotification from './components/NotivueNotification.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    NotivueNotification
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import router from './router'
 import studentIcon from './assets/student_icon.png'
+import { createNotivue } from 'notivue'
+import 'notivue/notifications.css'
+import 'notivue/animations.css'
 
 const link = document.createElement('link')
 link.rel = 'icon'
 link.href = studentIcon
 document.head.appendChild(link)
 
+const notivue = createNotivue()
+
+
 const app = createApp(App)
 app.use(router)
+app.use(notivue)
 app.mount('#app')
 
