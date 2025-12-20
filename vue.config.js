0551f489dac96f03432c3794cddf8256a7ed09e5
Jarosław Romańczuk <53583288+jarrobots@users.noreply.github.com>
2025-12-20 21:18:42 +0100

    Update vue.config.js
    changed .env

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
-const devHost = process.env.PRIF_HOST || '0.0.0.0';
+const devHost = process.env.PRIF_HOST || 'localhost';
 const devPort = Number(process.env.PRIF_PORT || 3000);
-const devAllowedHosts = process.env.PRIF_ALLOWED_HOSTS || 'all';
+const devAllowedHosts = process.env.PRIF_ALLOWED_HOSTS || 'localhost';
 const backendUrl = process.env.PRIB_URL || 'http://localhost:8082';
+
+console.log('devHost:', devHost, 'devPort:', devPort, 'backendUrl:', backendUrl);
 
 module.exports = {
   devServer: {
